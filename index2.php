35bc143de71f02a2d4899784d9c252356a6e3120
dinesh <soni.dineshds2000@gmail.com>
2020-10-01 00:40:37 +0530

    Added order summary using bootstrap grid

--- index2.php
+++ index2.php
@@ -4,195 +4,67 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Shopping Cart</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
-  <!-- <link rel="stylesheet" href="index1.css"> -->
-  <style>
-    .cardcontainer {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      /* justify-content: center; */
-      justify-content: space-around;
-      /* border: 2px solid blue; */
 
-    }
-
-    .container-fluid {
-      padding-left: 100px;
-      padding-right: 100px;
-
-    }
-
-    /* @media screen and(min-width: 1200px){ */
-    .card1 {
-      width: 19vw;
-      height: 10%;
-      /* border: 1px solid #929693; */
-    }
-
-    .image1 {
-      height: 23.2vw;
-    }
-
-
-    /* }  */
+  <link rel="stylesheet" href="./cart.css">
+  <!-- <style>
     @font-face {
       font-family: myFirstFont;
       src: url("Fonts/Montserrat-Regular.ttf");
     }
-
-    @media screen and (max-width: 768px) {
-      .container-fluid {
-        padding: 0 0 0 0px;
-      }
-
-      .cardcontainer {
-
-        justify-content: center;
-        /* justify-content: flex-start; */
-        /* justify-content: space-between; */
-        /* border: 2px solid blue; */
-
-      }
-
-      .card1 {
-        width: 50%;
-      }
-
-      .image1 {
-        height: 50vw;
-      }
-    }
-
-    @media screen and (max-width:600px) {
-      .image1 {
-        height: 62vw;
-      }
-    }
-  </style>
+    </style> -->
 </head>
 
 <body>
+  <div class="container-fluid">
+    <div class="row mt-5">
+      <div class="col-md-8 box">Items</div>
+      <div class="col-md-4 p-4 box">
+        <h4>Order Summary</h4>
+        <hr style="border-radius:100px;width:94%;margin-left:0px;background-color:#FF9933;height:1.5px;">
+        <div class="mt-4 p-4">
+          <h5 class="mt-4 d-inline">Price(3 item):</h5>
+          <p class="font-weight-bold mt-4 d-inline" style="font-size: 19px;"><i style="font-size:17px;" class="fas fa-rupee-sign"></i> 30,000 </p>
+          <br>
+          <h5 class="mt-4 d-inline">Delivery Charges:</h5>
+          <p class="font-weight-bold d-inline" style="font-size: 19px;"> <i style="font-size:17px;" class="fas fa-rupee-sign"></i> 75 </p>
 
-  <div class="container-fluid cardcontainer mt-4">
+          <div class="input-group mt-4">
+            <input type="text" class="form-control" placeholder="Have a coupon code.">
+            <div class="input-group-append">
+              <button style="background-color: #FF9933;" class="btn" type="button">Apply</button>
+            </div>
+          </div>
+        </div>
+        <hr style="border-radius:100px;width:94%;margin-left:0px;background-color:#FF9933;height:1.5px;">
+        <h4 class="d-inline">Sub Total:</h4>
+        <p class="font-weight-bold d-inline" style="font-size: 23px;"> <i style="font-size:19px;" class="fas fa-rupee-sign"></i> 75 </p>
+        <div style="background-color: #ff9933;height:50px" class="mt-4 d-block btn"><span style="font-size: 21px;" class="font-weight-bold">Place Order</span></div>
 
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images1.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
+        <div class="row mt-4 ml-1">
+          <div class="column">
+            <img src="images/ruppee.png" style="height: 43px;width:55px;" alt="">
+          </div>
+          <div class="ml-3 column">
+            <h5 class="row mb-0">MONEY BACK GUARANTEE </h5>
+            <h6 class="row">100% money back guarantee.</h6>
+          </div>
+        </div> 
 
-        <h3 class="card-title font-weight-bold ct" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#F71616B3;border-radius:10px;width:100px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">LIMITED !!<span></p>
+        <div class="row mt-3 ml-1">
+          <div class="column">
+            <img src="images/lock.png" style="height: 43px;width:55px;" alt="">
+          </div>
+          <div class="ml-3 column">
+            <h5 class="row mb-0">SECURE PAYMENTS </h5>
+            <h6 class="row">Cards,Netbanking,Wallets,COD,UPI.</h6>
+          </div>
+        </div> 
+
       </div>
     </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images2.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#64ed88;border-radius:10px;width:70px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">NEW !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images3.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#ffa436;border-radius:10px;width:70px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">FEW !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images4.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#5294ff;border-radius:10px;width:80px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">BEST !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images1.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#F71616B3;border-radius:10px;width:100px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">LIMITED !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images2.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#64ed88;border-radius:10px;width:70px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">NEW !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images3.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#ffa436;border-radius:10px;width:70px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">FEW !!<span></p>
-      </div>
-    </div>
-    <div class="card card1" style="margin-top:20px;">
-      <img class="card-img-top image1" src="./images/images4.jpg" alt="">
-      <div class="card-body" style="margin:0px;">
-
-        <h3 class="card-title font-weight-bold" style="margin-bottom:3px;font-size:18px;font-family:myFirstFont;">T-shirt medium size</h3>
-        <!-- <p class="card-text">
-  This is the coder t-shirt good cotton,premium quality.
-</p>  -->
-        <h6 class="d-inline card-subtitle" style="font-size:18px;font-family:myFirstFont;"><i class="fas fa-rupee-sign"></i>799
-        </h6>
-        <h6 class="text-muted ml-1 d-inline card-subtitle" style="font-size:14px;font-family:myFirstFont;text-decoration: line-through;"><i class="fas fa-rupee-sign"></i>1499</h6>
-        <p class="font-weight-bold text-center mt-3" style="margin-bottom:0px;background-color:#5294ff;border-radius:10px;width:80px;color:white;"><span style="font-family:myFirstFont;font-size:13px;">BEST !!<span></p>
-      </div>
-    </div>
-
-
-
   </div>
 </body>
 
